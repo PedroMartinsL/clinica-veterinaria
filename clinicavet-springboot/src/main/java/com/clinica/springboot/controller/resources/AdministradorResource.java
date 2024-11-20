186014bea81517c70938939f573df64d232f8bf0
Pedro Martins <pedromartinsdelemos@gmail.com>
2024-11-20 10:47:36 -0300

    Administrador e services/resources/repository

---
+++ clinicavet-springboot/src/main/java/com/clinica/springboot/controller/resources/AdministradorResource.java
@@ -0,0 +1,60 @@
+package com.clinica.springboot.controller.resources;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.clinica.springboot.controller.services.AdministradorService;
+import com.clinica.springboot.model.entities.Administrador;
+
+@RestController
+@RequestMapping(value = "/adms")
+public class AdministradorResource {
+	
+	@Autowired
+	private AdministradorService service;
+		
+	@GetMapping 
+	public ResponseEntity<List<Administrador>> findAll() {
+		List<Administrador> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}") 
+	public ResponseEntity<Administrador> findById(@PathVariable String id) {
+		Administrador obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Administrador> insert(@RequestBody Administrador obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getCpf()).toUri();
+		return ResponseEntity.created(uri).body(obj); 
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id)  {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+		//código 204, quando não temos conteúdo
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Administrador> update(@PathVariable String id, @RequestBody Administrador obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+}
---
+++ clinicavet-springboot/src/main/java/com/clinica/springboot/controller/services/AdministradorService.java
@@ -0,0 +1,63 @@
+package com.clinica.springboot.controller.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.clinica.springboot.controller.exceptions.DatabaseException;
+import com.clinica.springboot.controller.exceptions.ResourceNotFoundException;
+import com.clinica.springboot.controller.repositories.AdministradorRepository;
+import com.clinica.springboot.model.entities.Administrador;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class AdministradorService {
+	
+	@Autowired
+	private AdministradorRepository repository;
+
+	public List<Administrador> findAll() {
+		return repository.findAll();
+	}
+
+	public Administrador findById(String id) {
+		Optional<Administrador> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id)); // retorna o objeto do tipo Administrador que estiver																	// dentro do optional
+	}
+
+	public Administrador insert(Administrador obj) {
+		return repository.save(obj);
+	}
+
+	public void delete(String id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage()); //Passandp exception da própria camada de serviço
+		}
+	}
+
+	public Administrador update(String id, Administrador obj) {
+		try {
+			Administrador entity = repository.getReferenceById(id); //monitorar um obj pelo jpa para depois efetuar uma op no bd
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Administrador entity, Administrador obj) {
+		entity.setCpf(obj.getCpf());
+		entity.setNome(obj.getNome());
+		entity.setSenha(obj.getSenha());
+		
+	}
+}
