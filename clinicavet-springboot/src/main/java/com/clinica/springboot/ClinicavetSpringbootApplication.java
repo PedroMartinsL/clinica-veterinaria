2e09e71a266dba311c54591f32a34e162cb0c228
Pedro Martins <pedromartinsdelemos@gmail.com>
2024-11-20 00:25:54 -0300

    Criando projeto spring boot para clínica

---
+++ clinicavet-springboot/src/main/java/com/clinica/springboot/ClinicavetSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.clinica.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicavetSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicavetSpringbootApplication.class, args);
+	}
+
+}
---
+++ clinicavet-springboot/src/test/java/com/clinica/springboot/ClinicavetSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicavetSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
