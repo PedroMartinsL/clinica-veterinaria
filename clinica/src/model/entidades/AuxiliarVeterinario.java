712205387d056d5667d1a595f768c9b0aba2e599
jamilefreitassdev <vitoria.jafreitas@upe.br>
2024-11-21 22:45:57 -0300

    Atualização de métodos e implementação no banco de dados.
    Atualizando os métodos de auxveterinário

--- clinica/src/model/entidades/AuxiliarVeterinario.java
+++ clinica/src/model/entidades/AuxiliarVeterinario.java
@@ -1,16 +1,26 @@
 package model.entidades;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 
 import java.util.Map;
 import java.util.Scanner;
+
+import db.DB;
+import db.DbException;
 import model.recursos.Medicamento;
 import model.recursos.Pedido;
+import view.UI;
 
 
-public class AuxiliarVeterinario extends Entidade {
+public class AuxiliarVeterinario extends Veterinario {
 	
 	Scanner sc = new Scanner(System.in);
+	
+	static Connection conn = DB.getConnection();
 	
 	public Consulta getConsulta() {
 		return consulta;
@@ -28,19 +38,12 @@ public class AuxiliarVeterinario extends Entidade {
 	}
 
 	@Override
-	public void operacoes() {
-
-		while (true) {
-			System.out.println("Menu de operações (Auxiliar Veterinário)");
-			System.out.println("1. Atender Pet");
-			System.out.println("2. Aplicar Medicamento");
-			System.out.println("3. Sair");
-			System.out.print("Escolha uma opção: ");
-			int opcao = sc.nextInt();
-			sc.nextLine();
-
-			switch (opcao) {
-			case 1:
+	public boolean operacoes() {
+		int request = UI.getRequest(new String[] { "Operações de contratados", "Exibir renda da clínica",
+				"Operações de Estoque", "Exibir notificações", "Finalizar operações" });
+		
+		switch (request) {
+		case 1:
 				System.out.println("Atendimento ao Pet realizado.");
 				break;
 
@@ -49,18 +52,50 @@ public class AuxiliarVeterinario extends Entidade {
 				break;
 
 			case 3:
-				System.out.println("Saindo...");
-				sc.close();
-				return;
+				  System.out.println("Saindo das operações...");
+	              break; 
+	          default:
+	              return false;
+	      }
+		return true;
+	}
 
-			default:
-				System.out.println("Opção inválida! Tente novamente.");
+	public static void listarConsultas() {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			st = conn
+					.prepareStatement("SELECT * FROM Consultas");
+			rs = st.executeQuery();
+
+			System.out.println("Medicamentos disponíveis:");
+			System.out.printf("%-20s %-10s %-15s %-15s %-10s %-10s%n", "Nome", "Preço", "Laboratório", "Quantidade",
+					"Concentração", "Contrato");
+			System.out.println("-----------------------------------------------------------------------------------");
+
+			while (rs.next()) {
+				String nome = rs.getString("nome");
+				double preco = rs.getDouble("preco");
+				String laboratorio = rs.getString("laboratorio");
+				int quantidade = rs.getInt("quantidade");
+				double concentracao = rs.getDouble("concentracao");
+				boolean contrato = rs.getBoolean("contrato");
+
+				System.out.printf("%-20s %-10.2f %-15s %-15s %-10.2f %-10s%n", nome, preco, laboratorio, quantidade,
+						concentracao, contrato ? "Sim" : "Não");
 			}
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
 		}
 	}
 
-	void aplicarMedicamento(Medicamento nome, Entidade auxVeterinario) {
-
+	void aplicarMedicamento(Map<Medicamento, Integer> mapMedicamentos, Pet pet) {
+//recebe e vai aplicando
 		System.out.print("Informe o nome do medicamento: ");
 		String nomeMedicamento = sc.nextLine();
 
@@ -75,28 +110,27 @@ public class AuxiliarVeterinario extends Entidade {
 		Map<Medicamento, Integer> mapMedicamentos = new HashMap<>();
 		mapMedicamentos.put(medicamento, quantidadeMedicamento);
 
-		Pedido pedido = new Pedido(auxVeterinario, mapMedicamentos);
+		Pedido pedido = new Pedido(auxVeterinario aux, mapMedicamentos);
 
 		// solicitar medicamento
 		solicitarMedicamento(pedido);
 
 		System.out.println("Aplicando " + quantidadeMedicamento + " doses de " + nomeMedicamento
-				+ " para o pet do dono com CPF: ");
+				);
 	}
 
 	private void solicitarMedicamento(Pedido pedido) {
 		System.out.println("Medicamento solicitado: " + pedido);
+	
 
 	}
-
-	@Override
-	public void addUser() {
-
+	private static void finalizarConsulta() {
+		
 	}
-
-	@Override
-	public void removerUser() {
-
-	}
-
+	// atualizar o status da consulta para numero 3, em dívida
+	
+	//realizar atendimento: chama metodo que atualiza status, id, id veterinario, construir veterinsario só com cpf, chama operaão de veterinario
+	//Checar diagnostico e prescrever medicaçao, aplicar medicamentos e chaamae o metodo de atualizar status para divida
+	
+	
 }
--- clinica/src/model/entidades/Veterinario.java
+++ clinica/src/model/entidades/Veterinario.java
@@ -1,9 +1,17 @@
 package model.entidades;
 
-import java.util.List;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
+import db.DB;
+import db.DbException;
 import model.recursos.Medicamento;
+import model.recursos.Pedido;
+import view.UI;
 public class Veterinario extends Entidade {
 
 
@@ -14,94 +22,141 @@ public class Veterinario extends Entidade {
 	 Scanner sc = new Scanner(System.in);
 
 	@Override
-	public void operacoes() {
-  
-		 while (true) {
-	            System.out.println("Menu de operações (Veterinário)");
-	            System.out.println("1. Checar Diagnóstico");
-	            System.out.println("2. Prescrever Medicamento");
-	            System.out.println("3. Sair");
-	            System.out.print("Escolha uma opção: ");
-	            int opcao = sc.nextInt();
-	            sc.nextLine(); 
-
-	            switch (opcao) {
-	                case 1:
-	                    checagemDiagnostico(null);
-	                    break;
-
-	                case 2:
-	                    prescreverMedicacao(new AuxiliarVeterinario(null, null, null)); // Enviando o auxiliar como parâmetro
-	                    break;
-
-	                case 3:
-	                    System.out.println("Saindo...");
-	                    sc.close(); 
-	                    return; 
-	                default:
-	                    System.out.println("Opção inválida! Tente novamente.");
-	            }
-	        }
-	    }
-	            public void checagemDiagnostico(Pet pet) {
-	                System.out.print("Informe o sintoma do pet: ");
-	                String sintoma = sc.nextLine();
-	                pet.adicionarDoenca(sintoma);
-	                System.out.println("Sintoma '" + sintoma + "' adicionado ao pet.");
-
-	         
-	            }
-
-	            public void prescreverMedicacao(AuxiliarVeterinario auxiliar) {
-	                System.out.print("Informe o CPF do dono do pet: ");
-	                String cpfDono = sc.nextLine();
-
-	                Pet pet = buscarPetPorCpf(cpfDono);
-	                if (pet != null) {
-	                    System.out.print("Informe o nome do medicamento: ");
-	                    String nomeMedicamento = sc.nextLine();
-
-	                    System.out.print("Informe a concentração do medicamento (mg): ");
-	                    int concentracao;
-	                    try {
-	                        concentracao = Integer.parseInt(sc.nextLine()); 
-	                    } catch (NumberFormatException e) {
-	                        System.out.println("Concentração inválida, operação cancelada.");
-	                        return;
-	                    }
-
-	                    // Criação do objeto Medicamento
-	                    Medicamento medicamento = new Medicamento(0, "Laboratório", concentracao, nomeMedicamento, null, null);
-
-	                    System.out.printf("Medicamento %s de %d mg prescrito para o pet do dono com CPF: %s.\n", 
-	                                      medicamento.getNome(), medicamento.getConcentracao(), cpfDono);
-
-	                    // Chamando o auxiliar para aplicar o medicamento
-	                    if (auxiliar != null) {
-	                        auxiliar.aplicarMedicamento(medicamento, auxiliar);
-	                    }
-	                } else {
-	                    System.out.println("Pet não encontrado.");
-	                }
-	            }
-	        
-	@Override
-	public void addUser() {
-		
-		
+	public boolean operacoes() {
+		int request = UI.getRequest(new String[] { "Operações de contratados", "Exibir renda da clínica",
+				"Operações de Estoque", "Exibir notificações", "Finalizar operações" });
+		switch (request) {
+		case 1:
+			  checagemDiagnostico(null); 
+              break;
+          case 2:
+              prescreverMedicacao(new AuxiliarVeterinario(null, null, null), null); //auxiliar como parâmetro
+              break;
+          default: 
+        	  System.out.println("Saindo das operações...");
+      } 
+		return true;
 	}
 
-	@Override
-	public void removerUser() {
-		
+    public Map<Medicamento, Integer> prescreverMedicacao(int idConsulta ,AuxiliarVeterinario auxiliar, Pet pet) {
+        //instanciar map 
+        if (pet != null) {
+        	//while
+            System.out.print("Informe o nome do medicamento: ");
+            String nomeMedicamento = sc.nextLine();
+            
+            System.out.print("Informe o nome do medicamento: ");
+            String nomeLaboratorio = sc.nextLine();
+
+            System.out.print("Informe a concentração do medicamento (mg): ");
+            int concentracao;
+            try {
+                concentracao = Integer.parseInt(sc.nextLine()); 
+            } catch (NumberFormatException e) {
+                System.out.println("Concentração inválida, operação cancelada.");
+                return;
+            }
+
+            // Criação do objeto Medicamento
+            Medicamento medicamento = new Medicamento(nomeLaboratorio, concentracao, nomeMedicamento);
+
+            //fazer o put
+            System.out.printf("Medicamento %s de %d mg prescrito para o pet do dono com CPF: %s.\n", 
+                              medicamento.getNome(), medicamento.getConcentracao());
+            
+            //Deseja cadastrar outro medicamento?
+//termina while
+            //Construir pedido passando map, com id da consulta, e a entidade
+            // estoque.solicitar medicamento passa o pedido como parametro 
+            //estoque retorna map de remedios e recebe medicamento
+
+        } else {
+            System.out.println("Pet não encontrado.");
+        }
+    }
+
+	private void checagemDiagnostico(int idConsulta) {
+        System.out.print("Informe o sintoma do pet: ");
+        String sintoma = sc.nextLine();
+        // Fazer update da consulta na coluna doença pegando id da consulta
+        System.out.println("Sintoma '" + sintoma + "' adicionado ao pet.");
+
+
+	}
+	
+
+	public static void listarConsultas() {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			st = conn
+					.prepareStatement("SELECT * FROM Consultas");
+			rs = st.executeQuery();
+
+			System.out.println("Medicamentos disponíveis:");
+			System.out.printf("%-20s %-10s %-15s %-15s %-10s %-10s%n", "Nome", "Preço", "Laboratório", "Quantidade",
+					"Concentração", "Contrato");
+			System.out.println("-----------------------------------------------------------------------------------");
+
+			while (rs.next()) {
+				String nome = rs.getString("nome");
+				double preco = rs.getDouble("preco");
+				String laboratorio = rs.getString("laboratorio");
+				int quantidade = rs.getInt("quantidade");
+				double concentracao = rs.getDouble("concentracao");
+				boolean contrato = rs.getBoolean("contrato");
+
+				System.out.printf("%-20s %-10.2f %-15s %-15s %-10.2f %-10s%n", nome, preco, laboratorio, quantidade,
+						concentracao, contrato ? "Sim" : "Não");
+			}
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+
+	void aplicarMedicamento(Map<Medicamento, Integer> mapMedicamentos, Pet pet) {
+
+		System.out.print("Informe o nome do medicamento: ");
+		String nomeMedicamento = sc.nextLine();
+
+		System.out.print("Informe a quantidade do medicamento: ");
+		int quantidadeMedicamento = sc.nextInt();
+		sc.nextLine();
+
+		// construtor do medicamento
+		Medicamento medicamento = new Medicamento(0, nomeMedicamento, 0, null, null, null);
+
+		// armazena o medicamento e sua quantidade
+		Map<Medicamento, Integer> mapMedicamentos = new HashMap<>();
+		mapMedicamentos.put(medicamento, quantidadeMedicamento);
+
+		Pedido pedido = new Pedido(auxVeterinario, mapMedicamentos);
+
+		// solicitar medicamento
+		solicitarMedicamento(pedido);
+
+		System.out.println("Aplicando " + quantidadeMedicamento + " doses de " + nomeMedicamento
+				);
+	}
+
+	private void solicitarMedicamento(Pedido pedido) {
+		System.out.println("Medicamento solicitado: " + pedido);
+	
+
+	}
+	private static void finalizarConsulta() {
 		
 	}
-	 public Pet buscarPetPorCpf(String cpfDono) {
-	        for (Pet pet : listaDePets) {
-	            if (pet.getCpfDono().equals(cpfDono)) {
-	                return pet; // Retorna o pet se o CPF do dono coincidir
-	            }
-	        }
-	        return null; 
-	    }
+	// atualizar o status da consulta para numero 3, em dívida
+	
+	
+	//realizar atendimento: chama metodo que atualiza status, id, id aux veterinario, construir veterinsario só com cpf, chama operaão de veterinario
+		//Checar diagnostico e prescrever medicaçao, aplicar medicamentos e chaamae o metodo de atualizar status para divida
+
 }
+
